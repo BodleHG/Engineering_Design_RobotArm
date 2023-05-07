2ac09a09fc23826e5a91765b81520bd4760633d9
BodleHG <20181588@edu.hanbat.ac.kr>
2023-05-07 18:20:21 +0900

    .

--- mirobotApi/run.py
+++ mirobotApi/run.py
@@ -12,31 +12,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     print("미로봇 상태를 초기화합니다...")
-    arm = WlkataMirobot(portname=PORTNAME)
     
-    print(f"포트 연결이 감지되었습니다. 현재 포트 : {PORTNAME}")
+    try:
+        arm = WlkataMirobot(portname=PORTNAME)
+        print(f"포트 연결이 감지되었습니다. 현재 포트 : {PORTNAME}")
     
-    # Mirobot Arm Multi-axis executing
-    print("Mirobot Homing...")
+        # Mirobot Arm Multi-axis executing
+        print("Mirobot Homing...")
 
-    arm.home()
+        arm.home()
 
-    print("Homing finish")
-        
-    print("이펙터가 감지되었습니다. 현재 이펙터 : FLEXIBLE CLAW")
-    arm.set_tool_type(WlkataMirobotTool.FLEXIBLE_CLAW)
-    arm.pump_suction()
-    time.sleep(1)
-    arm.pump_blowing()
-    time.sleep(1)
-    arm.pump_off()
+        print("Homing finish")
+            
+        print("이펙터가 감지되었습니다. 현재 이펙터 : FLEXIBLE CLAW")
+        arm.set_tool_type(WlkataMirobotTool.FLEXIBLE_CLAW)
+        arm.pump_suction()
+        time.sleep(1)
+        arm.pump_blowing()
+        time.sleep(1)
+        arm.pump_off()
 
-    if args.mode == "keyboard":
-        print("현재 키보드 제어 모드입니다. 키보드를 통해 제어해주세요.")
-        # Keymanager(arm)._setJointAngle()
-        asyncio.run(Keymanager(arm)._setAxis())
-        
-    # elif args.mode == "remote":
-    #     print("Remote Control Mode")   
-    #     Remotemanager(arm).remoteRecv() 
+        if args.mode == "keyboard":
+            print("현재 키보드 제어 모드입니다. 키보드를 통해 제어해주세요.")
+            # Keymanager(arm)._setJointAngle()
+            asyncio.run(Keymanager(arm)._setAxis())
+            
+        # elif args.mode == "remote":
+        #     print("Remote Control Mode")   
+        #     Remotemanager(arm).remoteRecv() 
+    except:
+        print("포트 연결이 감지되지 않았습니다. 프로그램을 종료합니다.")
     
